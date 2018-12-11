a22bcb72db57daa2405a09c0979d5286a438338c
yourboijake <44953097+yourboijake@users.noreply.github.com>
2018-12-10 17:57:10 -0800

    Update source.cpp

--- source_code/source.cpp
+++ source_code/source.cpp
@@ -13,192 +13,95 @@
 #include <fstream>
 using namespace std;
 
-class Board{
-private:
-    char space[9]; //board values
-    char turn; //identifies whose turn it is
-    char minimaxturn; //AI's turn
-    char depth; //number of moves made in the game
-public:
-    Board(){
-        turn = 'X';
-        depth = 0;
-        for(int i=0;i<9;i++){
-            space[i] = '1'+i;
-        }
-    }
-
-//outputs the board
-    void displayBoard(){
-        cout << " " << space[0] << " | " << space[1] << " | " << space[2] << endl;
-        cout << "------------" << endl;
-        cout << " " << space[3] << " | " << space[4] << " | " << space[5] << endl;
-        cout << "------------" << endl;
-        cout << " " << space[6] << " | " << space[7] << " | " << space[8] << endl;
-    }
-
-//changes the square at index to char move
-    void changeSqr(int index, char move){
-        space[index-1] = move;
-    }
-
-//returns true if all the spaces are filled
-    bool spacesFilled(){
-        bool filled = true;
-        for(int i=0;i<9;i++){
-            if(space[i]=='1'+i)
-                filled = false;
-        }
-        return filled;
-    }
-
-//tests if making a move at the index input would be legal or not
-    bool isLegal(int index){
-        if(getSpace(index)==index+'1')
-            return true;
-        else
-            return false;
-    }
-
-//return true if someone has won the game
-    bool gameWon(){
-        if(space[0]==space[1]&&space[2]==space[1])
-            return true;
-        else if(space[3]==space[4]&&space[5]==space[4])
-            return true;
-        else if(space[6]==space[7]&&space[7]==space[8])
-            return true;
-        else if(space[0]==space[3]&&space[6]==space[3])
-            return true;
-        else if(space[1]==space[2]&&space[7]==space[4])
-            return true;
-        else if(space[2]==space[5]&&space[8]==space[5])
-            return true;
-        else if(space[0]==space[4]&&space[4]==space[8])
-            return true;
-        else if(space[2]==space[4]&&space[4]==space[6])
-            return true;
-        else
-            return false;
-    }
-
-//evaluates if a position is won or not, and returns the score of the position
-    int evalPosition(char AIchar){
-        if(gameWon()&&turn==AIchar)
-            return 10;
-        else if(gameWon()&&turn!=AIchar)
-            return -10;
-        else
-            return 0;
-    }
-//returns a char for who won the game, returns C if its a cat game
-    char getWinner(){
-        if(gameWon())
-            return turn;
-        else if(spacesFilled())
-            return 'C';
-    } 
-    void incrementDepth(){depth++;}
-    int getDepth(){return depth;}
-    char getSpace(int index){return space[index];}
-    char getTurn(){return turn;}
-    void setAI(char c){minimaxturn = c;}
-    char getMinimaxTurn(){return minimaxturn;}
-    char getNotMinimaxTurn(){
-        if(minimaxturn=='X')
-            return 'Y';
-        else
-            return 'X';
-    }
-    void switchPlayer(){
-        if(turn=='X')
-            turn = 'Y';
-        else
-            turn = 'X';
+struct Move{
+    int score;
+    int index_best;
+    Move(){
+        score = 0;
+        index_best = 0;
     }
 };
 
-int minimax(Board b, bool isAI, int depth){
+int moveScore(Board b, int score_count, bool isAI, int depth){
     int score = b.evalPosition(b.getMinimaxTurn());
-    if(depth > 1000){
+    if(depth>10000)
         return 0;
-        depth = 0;
-    }
-    else if(score==10)
-        return score;
-    else if(score==-10)
-        return score;
-    else if(b.spacesFilled())
+    else if(score==1)
+        return 1;
+    else if(score==-1)
+        return -1;
+    else if(score==0)
         return 0;
     else if(isAI){
-        int best = -1000;
         for(int i=0;i<9;i++){
             if(b.isLegal(i)){
-                b.changeSqr(i+1, b.getMinimaxTurn());
+                b.changeSqr(i+1,b.getMinimaxTurn());
                 depth++;
-                best = max(best, minimax(b, !isAI, depth));
+                score_count += moveScore(b, score_count, !isAI, depth);
                 b.changeSqr(i+1,i+'1');
+                b.displayBoard();
             }
         }
-        return best;
+        return score_count;
     }
     else{
-        int best = 1000;
         for(int i=0;i<9;i++){
             if(b.isLegal(i)){
-                b.changeSqr(i+1, b.getNotMinimaxTurn());
+                b.changeSqr(i+1,b.getNotMinimaxTurn());
                 depth++;
-                best = min(best, minimax(b, !isAI, depth));
+                score_count += moveScore(b, score_count, !isAI, depth);
                 b.changeSqr(i+1,i+'1');
+                b.displayBoard();
             }
         }
-        return best;
+        return score_count;
     }
 }
 
-int findBestMove(Board b){
-    int bestVal = -1000;
-    int index_best;
-    int depth = 0;
+Move findBestMove(Board b){
+    Move m1;
+    bool isAI = true;
+    m1.score = -1000;
     for(int i=0;i<9;i++){
         if(b.isLegal(i)){
             b.changeSqr(i+1,b.getMinimaxTurn());
-            int moveVal = minimax(b, false, depth);
-            b.changeSqr(i+1,i+'1');
-            if(moveVal>bestVal){
-                bestVal = moveVal;
-                index_best = i;
+            int tempscore = moveScore(b, 0, !isAI, 0);
+            if(tempscore>m1.score){
+                m1.score = tempscore;
+                m1.index_best = i;
             }
+            b.changeSqr(i+1,i+'1');
         }
     }
-    return index_best;
+    return m1;
 }
 
 int main(){
-    Board b;
+    Board b, b2;
     char player;
     cout << "do you want to be X's or O's? Type 'X' or 'O':  ";
     cin >> player;
     
-    if(player=='X')
+    if(player=='X') {
         b.setAI('O');
-    else
+        b2.setAI('O');
+    }
+    else {
         b.setAI('X');
+        b2.setAI('X');
+    }
 
-    int move;
-    bool legit_move=false;
-    int AImove;
-
-    Board b2;
     b2.changeSqr(1,'X');
-    b.switchPlayer();
+    b2.changeSqr(2,'X');
+    b2.changeSqr(3,'O');
     b2.changeSqr(4,'O');
-    b.switchPlayer();
+    b2.changeSqr(5,'O');
     b2.changeSqr(7,'X');
-    b.switchPlayer();
+    b2.changeSqr(8,'X');
     b2.displayBoard();
-    int best_move = findBestMove(b2)+1;
-    cout << endl << "The best move in this position is " << best_move;
+    Move best = findBestMove(b2);
+    cout << endl << "The best move in this position for O is " << best.index_best+1 << endl;
+
     /*do{
         b.displayBoard();
         if(b.getTurn()==player){
